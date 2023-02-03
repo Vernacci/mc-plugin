7a9eec1fe203e25751b18b5cb39c05683cb4d7fb
Victor Vernasqui <victorvernasqui12@gmail.com>
2023-02-02 22:53:08 -0300

    Update Vmod.java

--- src/main/java/br/vernacci/vmod/Vmod.java
+++ src/main/java/br/vernacci/vmod/Vmod.java
@@ -1,7 +1,6 @@
 package br.vernacci.vmod;
 
 import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Vmod extends JavaPlugin {
