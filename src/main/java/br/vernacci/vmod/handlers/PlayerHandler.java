aa6d57a224149ca37e44c0abf51ce2a42156a976
Victor Vernasqui <victorvernasqui12@gmail.com>
2023-02-04 13:42:24 -0300

    Update PlayerHandler.java

--- src/main/java/br/vernacci/vmod/handlers/PlayerHandler.java
+++ src/main/java/br/vernacci/vmod/handlers/PlayerHandler.java
@@ -1,9 +1,7 @@
 package br.vernacci.vmod.handlers;
 
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
+
 
 public class PlayerHandler implements Listener {
 
